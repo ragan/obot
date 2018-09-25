4eb8c8df23c0b033760fbd934b8c5c33d9a47945
Karol Peek <karol.peek@grupawp.pl>
2018-09-25 10:55:59 +0200

    logger

--- main.go
+++ main.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"log"
+)
+
+var logger *log.Logger
+
 func main() {
-	println("test")
+	logger = log.New(os.Stdout, "f", log.LstdFlags)
+
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	flagP := flag.Int("p", 0, "Amount percent")
+	flag.Parse()
+
+	if *flagP == 0 {
+		return fmt.Errorf("\"p\" value is %d", *flagP)
+	}
+
+	rdr := bufio.NewReader(os.Stdin)
+	scanner := bufio.NewScanner(rdr)
+	for scanner.Scan() {
+		do(scanner.Text())
+	}
+	println(*flagP)
+	return nil
+}
+
+func do(s string) {
+	logger.Println(s)
 }
